7446a88be63b42cd4cd9c88c3af00302a17d35da
Samuel Ngwarai <samuel.ngwarai@figo.io>
2021-07-06 23:15:02 +0200

    try default url

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git branch -M main
+# git branch -M main
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:Samuel-Ngwarai/lets-connect.git main
