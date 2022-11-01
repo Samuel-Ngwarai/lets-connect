a60dc111462971d28794dbf7134c4d498a948b70
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2022-11-01 22:53:02 +0100

    add about section template

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <TheHero />
+    <AboutSection />
   </div>
 </template>
 
---
+++ components/AboutSection.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="h-screen bg-skyblue grid grid-cols-2 pt-20 px-10">
+    <div>
+        <p></p>
+    </div>
+    <div>
+      <p class="text-6xl text-black">About Me</p>
+      <p class="text-xl py-5">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in mi
+        nec diam mattis ornare quis ut turpis. Class aptent taciti sociosqu ad
+        litora torquent per conubia nostra, per inceptos himenaeos. Mauris
+        laoreet nulla neque, non egestas eros malesuada sed. Donec sed hendrerit
+        felis. In facilisis nunc quis dolor suscipit mattis. Vivamus maximus
+        tristique ultricies. Morbi dignissim neque id quam mollis mattis.
+        Curabitur lacinia dolor et lorem tristique tristique.
+      </p>
+      <p class="text-xl py-5">
+        Aliquam fringilla placerat nulla vel rhoncus. Mauris porta aliquam nibh
+        ac euismod. Phasellus lobortis, mauris et dictum cursus, ex ligula
+        dapibus risus, ut tempus augue dui non dui. Donec scelerisque sed nibh
+        et eleifend. Curabitur risus lorem, porta non rhoncus vel, eleifend
+        vitae purus. Etiam ac auctor purus, quis blandit dolor. Donec ac sodales
+        enim. In ut viverra turpis, at iaculis risus. Maecenas eget ultrices mi.
+        Nulla facilisi. Phasellus sed risus tristique, bibendum turpis ut,
+        consectetur tortor. Quisque facilisis mollis sodales. Curabitur ut elit
+        sed sem fermentum imperdiet. Duis libero augue, malesuada ut risus sed,
+        mattis condimentum lorem. Vestibulum fringilla purus felis, non
+        tristique quam bibendum id. Etiam viverra metus eu feugiat bibendum.
+      </p>
+    </div>
+  </div>
+</template>
