5e87032dc466626c4c5cc8fc25c26ad5ce7514ca
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2022-11-06 00:36:11 +0100

    defer google fonts load

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,8 +14,8 @@ export default defineNuxtConfig({
           { name: 'description', content: 'Virtual coffee with Samuel Ngwarai' }
         ],
         link: [
-          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
-          { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
+          { onload: "this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')", as: 'style', rel: 'preload', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
+          { onload: "this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')", as: 'style', rel: 'preload', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
           { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
         ],
       }
