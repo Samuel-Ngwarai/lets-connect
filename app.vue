d1a71a427db9934c75fd1e1f537f20be8985a6be
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2022-12-07 18:09:00 +0100

    update packages, use default nuxt modules for tailwind and google fonts

--- app.vue
+++ app.vue
@@ -7,10 +7,6 @@
   </div>
 </template>
 
-<script lang="ts" setup>
-import "./assets/css/tailwind.css";
-</script>
-
 <style>
 html {
   scroll-behavior: smooth;
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    target: 'static',
     css: [
         '~/assets/css/main.css'
     ],
@@ -9,32 +8,25 @@ export default defineNuxtConfig({
         htmlAttrs: {
           lang: 'en'
         },
-        title: 'Samuel N',
+        title: 'Samuel Ngwarai',
         meta: [
-          { name: 'description', content: 'Virtual coffee with Samuel Ngwarai' }
+          { name: 'description', content: 'Connect with Samuel Ngwarai' }
         ],
         link: [
-          { onload: "this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')", as: 'style', rel: 'preload', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
-          { onload: "this.onload=null; this.rel='stylesheet'; document.body.classList.add('fontLoaded')", as: 'style', rel: 'preload', href: 'https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@100;300;400;500;700&display=swap', fetchpriority: 'low' },
-          { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+         { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
         ],
-        script: [
-          { src: "https://www.googletagmanager.com/gtag/js?id=G-RPCLK5DLFF" },
-          { children: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-RPCLK5DLFF');"}
-        ]
       }
     },
     modules: [
         '@vueuse/nuxt',
-    ],
-    build: {
-        postcss: {
-          postcssOptions: {
-            plugins: {
-              tailwindcss: {},
-              autoprefixer: {},
+        '@nuxtjs/tailwindcss',
+        ['@nuxtjs/google-fonts', {
+            download: true,
+            prefetch: true,
+            families: {
+              Roboto: [100, 300],
+              "M+PLUS+Rounded+1c": true,
             },
-          },
-        },
-    },
+        },],
+    ],
 });
