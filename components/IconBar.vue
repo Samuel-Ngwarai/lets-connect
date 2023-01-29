a3c0d3bc71f1fd41cde505bc54d50af8a8422e49
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2023-01-29 23:15:09 +0100

    add icon bar

---
+++ components/IconBar.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="absolute top-1/2 right-10">
+        <div>
+            <BasicLink href="https://github.com/Samuel-Ngwarai"><Icon name="uil:github" size="40px" class="text-red"/></BasicLink>
+        </div>
+    </div>
+</template>
--- components/TheHero.vue
+++ components/TheHero.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <IconBar />
     <HeroBackground></HeroBackground>
     <div class="absolute top-0 left-0 grid grid-rows-[3fr_2fr] h-screen w-screen md:w-5/12 md:place-content-center md:grid-rows-[7fr_6fr]">
       <div></div>
