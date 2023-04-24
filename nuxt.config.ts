934202bb4a9c9c8b95c20da46f88057d513572c6
Samuel Ngwarai <samuel.ngwarai@share-now.com>
2023-04-24 14:09:08 +0200

    add vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,35 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    css: [
-        '~/assets/css/main.css'
-    ],
-    app: {
-      head: {
-        htmlAttrs: {
-          lang: 'en'
-        },
-        title: 'Samuel Ngwarai',
-        meta: [
-          { name: 'description', content: 'Connect with Samuel Ngwarai' }
-        ],
-        link: [
-         { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-        ],
-      }
-    },
-    modules: [
-        '@vueuse/nuxt',
-        '@nuxtjs/tailwindcss',
-        'nuxt-icon',
-        ['@nuxtjs/google-fonts', {
-            download: true,
-            prefetch: true,
-            families: {
-              Roboto: [100, 300],
-              "M+PLUS+Rounded+1c": true,
-            },
-        },],
-    ],
+  css: [
+    '~/assets/css/main.css'
+  ],
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
+      title: 'Samuel Ngwarai',
+      meta: [
+        { name: 'description', content: 'Connect with Samuel Ngwarai' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      ],
+    }
+  },
+  modules: [
+    '@vueuse/nuxt',
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    ['@nuxtjs/google-fonts', {
+      download: true,
+      prefetch: true,
+      families: {
+        Roboto: [100, 300],
+        "M+PLUS+Rounded+1c": true,
+      },
+    },],
+  ],
+  plugins: [{ src: '~/plugins/vercel.ts', mode: 'client' }],
+
 });
